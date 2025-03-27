14809776020cf6febdf1b682c6a214407893e66a
Arun <arun.r@riverstonetech.com>
2025-03-27 19:42:50 +0530

    Added the contact number feature

--- mohan_impex/mohan_impex/customer.py
+++ mohan_impex/mohan_impex/customer.py
@@ -3,16 +3,16 @@ from frappe.model.workflow import apply_workflow
 
 @frappe.whitelist()
 def update_kyc_status(customer, unv_customer=None):
-    frappe.db.set_value("Unverified Customer", unv_customer, {
-        "customer": customer,
-        "kyc_status": "Completed"
-    })
-    filters = {
+    or_filters = {
         "customer": customer
     }
     if unv_customer:
-        filters.update({"unv_customer": unv_customer})
-    cvm_list = frappe.db.get_all("Customer Visit Management", filters, ["name"], pluck="name")
+        frappe.db.set_value("Unverified Customer", unv_customer, {
+            "customer": customer,
+            "kyc_status": "Completed"
+        })
+        or_filters.update({"unv_customer": unv_customer})
+    cvm_list = frappe.db.get_all("Customer Visit Management", or_filters=or_filters, fields=["name"], pluck="name")
     for cvm in cvm_list:
         frappe.db.set_value("Customer Visit Management", cvm, "kyc_status", "Completed")
 
@@ -22,6 +22,6 @@ def updated_workflow_state(self, status):
 
 def validate_dup_unv_id(self, status):
     if self.unv_customer:
-        cus_id = frappe.db.exists("Customer", {"unv_customer": self.unv_customer})
+        cus_id = frappe.db.exists("Customer", {"unv_customer": self.unv_customer, "name": ["!=", self.name]})
         if cus_id:
             frappe.throw(f"Customer {cus_id} has already been created for the Unverified Customer {self.unv_customer}")
