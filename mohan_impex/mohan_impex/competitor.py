e90e0d7e3deefcbe7feff7a4ee30fc37d5856147
Arun <arun.r@riverstonetech.com>
2025-06-03 21:35:38 +0530

    Trial timing check in and out

--- mohan_impex/mohan_impex/competitor.py
+++ mohan_impex/mohan_impex/competitor.py
@@ -2,5 +2,5 @@ import frappe
 
 
 def add_others_in_competitor_item(doc, method):
-    if not any(d.item_name == "Others" for d in doc.competitor_item):
+    if not any(d.item_name == "Other" for d in doc.competitor_item):
         doc.append("competitor_item", {"item_name": "Other"})
