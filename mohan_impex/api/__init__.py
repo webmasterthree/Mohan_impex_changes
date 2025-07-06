627b0ab29f91ec7551ffd7d91efa0c40fca70f42
Arun <arun.r@riverstonetech.com>
2025-07-06 11:56:51 +0530

    Puh notifcation changes

--- mohan_impex/api/__init__.py
+++ mohan_impex/api/__init__.py
@@ -669,7 +669,6 @@ def add_notification_from_comment(doc, method):
                         "read": 0,
                         "subject": title,
                         "email_content": body,
-                        "type": doc.get("comment_type"),
                         "document_type" : doc.reference_doctype,
                         "document_name": doc.reference_name
                     })
@@ -680,7 +679,7 @@ def add_notification_from_comment(doc, method):
 def send_notification(doc, method):
     doctype_lists = ["Customer Visit Management", "Trial Plan", "Trial Target", "Sample Requisition", "Sales Order", "Issue", "Marketing Collateral Request", "Customer", "Journey Plan"]
     if doc.document_type in doctype_lists:
-        frappe.get_value("User", doc.for_user, "full_name")
+        role_profile = frappe.get_value("User", doc.for_user, "role_profile")
         if doc.for_user and doc.for_user != "Administrator":
             device_tokens = frappe.get_all("Push Notification Device", filters={"user": doc.for_user, "disabled": 0}, pluck="device_token")
             soup = BeautifulSoup(doc.subject, "html.parser")
@@ -688,7 +687,8 @@ def send_notification(doc, method):
             body = doc.email_content
             data = {
                 "type": doc.document_type,
-                "id": doc.document_name
+                "id": doc.document_name,
+                "role_profile": role_profile
             }
             for device_token in device_tokens:
                 send_push_notification(device_token, title, body, data)
